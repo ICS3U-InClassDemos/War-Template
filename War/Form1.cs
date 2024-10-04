b6cf40e05e0023a8571674da2e7f7454ecbf0251
Tony Theodoropoulos <theodot@outlook.com>
2024-10-04 11:00:13 -0400

    demo completed

--- War/Form1.cs
+++ War/Form1.cs
@@ -8,18 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-//header info
+//A simple card war game
+//Created by: Mr. T.
+//Date: October 2024
 
 namespace War
 {
     public partial class Form1 : Form
     {
         //random generator for picking new cards   
+        Random randomGenerator = new Random();
 
         //variables to hold card values 
+        int playerCard, cpuCard;
 
         //variables to hold scores, initialized to 0
-
+        int playerScore = 0;
+        int cpuScore = 0;
 
         public Form1()
         {
@@ -33,23 +38,35 @@ namespace War
             cpuCardLabel.Image = null;
 
             //get random value between 1 and 10 for both player and cpu
+            playerCard = randomGenerator.Next(1, 11);
+            cpuCard = randomGenerator.Next(1, 11);
 
-            
             //display card values to the labels
-
+            playerCardLabel.Text = $"{playerCard}";
+            cpuCardLabel.Text = $"{cpuCard}";
 
             // if player value is greater then cpu card
             //    add to player score 
             //    show new player score 
             //    show message saying player won
+            if (playerCard > cpuCard)
+            {
+                playerScore++;
+                outputLabel.Text = $"Player wins";
+                playerScoreLabel.Text = $"{playerScore}";
+            }
+            
+            if (cpuCard > cpuScore) 
+            {
+                cpuScore++;
+                outputLabel.Text = "Computer wins";
+                cpuScoreLabel.Text = $"{cpuScore}";
+            }
               
-            // if player value is less then cpu card
-            //    add to cpu score
-            //    show new cpu score
-            //    show message saying cpu won
-              
-            // if player value is equal to cpu value 
-            //    show message that this round is a tie.                  
+            if (cpuCard == playerCard)
+            {
+                outputLabel.Text = "It's a tie";
+            }
 
         }
     }
